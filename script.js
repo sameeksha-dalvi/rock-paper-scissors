6a6005e38a908ae1fcf36352342bfe44c5d3fef1
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2025-04-11 20:40:39 +0530

    Add function getComputerChoice in script.js
    The function getComputerChoice will randomly return one of these values i.e rock, paper or scissors.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hellllllo")
+
+//function to get random rock , paper or scissors
+
+function getComputerChoice(){
+    let randomNum = Math.floor(Math.random() * 3);
+    let computerChoice;
+    if( randomNum === 0){
+        computerChoice = "rock";
+    }else if( randomNum === 1){
+        computerChoice = "paper";
+    }else{
+        computerChoice = "scissors";
+    }
+
+    return computerChoice;
+
+}
+
+
+
+
+
+
