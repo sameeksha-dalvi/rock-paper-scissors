38849c653a423e7b246e4187d16e54698838dbcf
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2025-04-14 00:42:45 +0530

    Create function playRound to play a round of game.
    This function takes 2 parameters humanChoice and CompChoice and compares them as per thr rules of the game and calculates score by incrementing the score value of each player and declares the winner by comparing the scores.

--- script.js
+++ script.js
@@ -29,7 +29,52 @@ function getComputerChoice(){
 function getHumanChoice(){
     let userChoice = prompt("Please enter your choice.");
     //console.log("User Input : "+userChoice);
+    return userChoice;
 }
 
 
+//function to play a single round of game
 
+function playRound(humanChoice, compChoice){
+    humanChoice = humanChoice.toLowerCase();
+console.log(" humanChoice "+ humanChoice);
+console.log(" compChoice "+ compChoice);
+
+ let message ="";
+if(humanChoice === 'rock' && compChoice === 'scissors'){
+     message = "Rock beats scissors !";
+    humanScore++;
+}else if(humanChoice === 'paper' && compChoice ==='rock' ){
+    message = "Paper beats rock !";
+    humanScore++;
+}else if( humanChoice === 'scissors' && compChoice ==='paper'){
+   message = "Scissors beat paper !";
+    humanScore++;
+}else if(humanChoice === 'rock' && compChoice ==='paper' ){
+    message = "Paper beats rock !";
+    computerScore++;
+}else if(humanChoice === 'paper' && compChoice ==='scissors' ){
+    message = "Scissors beat paper !";
+    computerScore++;
+}else if(humanChoice === 'scissors' && compChoice ==='rock' ){
+    message = "Rock beats scissors !";
+    computerScore++;
+}
+
+console.log(" Human Score : "+ humanScore);
+console.log(" Computer Score : "+ computerScore);
+
+if(humanScore > computerScore){
+    console.log("You Win! "+message);
+}else if(humanScore < computerScore){
+    console.log("You Lose! "+message);
+}else{
+    console.log("It's a Tie");
+}
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection,computerSelection);
